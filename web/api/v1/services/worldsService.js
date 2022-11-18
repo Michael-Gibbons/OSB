aa08d0051503adb6244256670fcfb9469862dfc4
Michael Gibbons <michael.gibbons92407@gmail.com>
2022-11-18 13:56:53 -0700

    fixing api bug

--- web/api/v1/services/worldsService.js
+++ web/api/v1/services/worldsService.js
@@ -15,7 +15,7 @@ const parameters = [
 
 
 //TODO: turn this into object with parameter object
-const getWorldById = () => {
+const getWorldById = (req, res, next) => {
   // hard coded data as example
   res.status(200).send({
     data: [{ id: '1', type: "thing", attributes: { mything: true } }, { id: '2', type: "thing", attributes: { mything: true } }],
@@ -23,7 +23,7 @@ const getWorldById = () => {
   });
 }
 
-const getWorldByName = () => {
+const getWorldByName = (req, res, next) => {
   // hard coded data as example
   res.status(200).send({
     data: [{ id: '1', type: "thing", attributes: { mything: true } }, { id: '2', type: "thing", attributes: { mything: true } }],
