6f93f088315df5090054cc165b334c5250d29404
Michael Gibbons <michael.gibbons92407@gmail.com>
2022-11-25 16:12:19 -0700

    updating comments removing test log

--- web/backend/routes/webhooks/shopify/order.js
+++ web/backend/routes/webhooks/shopify/order.js
@@ -1,7 +1,6 @@
 import Shopify from '../../../helpers/shopify-context.js'
 
 export function setupOrderWebhooks(path){
-  console.log(path)
   Shopify.Webhooks.Registry.addHandler("ORDERS_CREATE", {
     path,
     webhookHandler: async (topic, shop, body) => {
