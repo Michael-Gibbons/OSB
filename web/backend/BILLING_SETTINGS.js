7af9542a9ca409d3dd823cb9310913a80174eb84
Michael Gibbons <michael.gibbons92407@gmail.com>
2022-11-26 16:49:38 -0700

    cleaning and commenting

--- web/backend/BILLING_SETTINGS.js
+++ web/backend/BILLING_SETTINGS.js
@@ -1,3 +1,5 @@
+import { BillingInterval } from "./helpers/ensure-billing.js";
+
 // The transactions with Shopify will always be marked as test transactions, unless NODE_ENV is production.
 // See the ensureBilling helper to learn more about billing in this template.
 
--- web/backend/index.js
+++ web/backend/index.js
@@ -9,8 +9,6 @@ import Shopify from "./helpers/shopify-context.js";
 import applyAuthMiddleware from "./middleware/auth.js";
 import productCreator from "./helpers/product-creator.js";
 
-import { BillingInterval } from "./helpers/ensure-billing.js";
-
 import setSecurityPolicy from './middleware/set-security-policy.js';
 import createApiV1 from "./api/v1/config/createApiV1.js";
 import './redis/index.js'
@@ -28,16 +26,30 @@ import { BILLING_SETTINGS } from './BILLING_SETTINGS.js';
 export async function createServer() {
   const app = express();
 
+  // Makes Cookies accessible to the Shopify context.
   app.use(cookieParser(Shopify.Context.API_SECRET_KEY));
 
-  app.use("/", rootRouter)
-
-  await createApiV1(app)
-
+  // Defines /auth routes for Shopify's OAuth handshake
   applyAuthMiddleware(app, {
     billing: BILLING_SETTINGS,
   });
 
+  // Prevents iframes from being injected into the shopify admin.
+  app.use(setSecurityPolicy);
+
+  // Shopify Webhooks included in here, do not use body parsers on this router, ie this goes before express.json().
+  // If you're using routes that do require the express.json() middleware in here, simply apply it as needed directly on the route, making sure it is *not* applied to the shopify webhook route.
+  app.use("/", rootRouter)
+
+  // All endpoints after this point will have access to a request.body
+  // attribute, as a result of the express.json() middleware
+  app.use(express.json());
+
+  // Defines all routes created in /api. All Routes here will have access to req.body
+  // If a route is meant to be accessed from the shopify admin app interface, use the verifyRequest middleware on the route in question.
+  await createApiV1(app)
+
+  // TODO: remove
   app.get("/api/products/count", async (req, res) => {
     const session = res.locals.shopify.session
 
@@ -65,14 +77,12 @@ export async function createServer() {
     res.status(status).send({ success: status === 200, error });
   });
 
-  // All endpoints after this point will have access to a request.body
-  // attribute, as a result of the express.json() middleware
-  app.use(express.json());
 
-  app.use(setSecurityPolicy);
-
+  // Compresses and serves an optimized frontend bundle instead of the vite dev bundle if NODE_ENV is production
   applyProductionMiddleware(app)
 
+  // Anything not handled by this point will be redirected to /api/auth for OAuth verification.
+  // Or simply rejected with a error if not applicable
   app.use("/*", catchAllHandler);
 
   return { app };
