82b75e15b26e68b0922ba654ca471a9d249462d9
Michael Gibbons <michael.gibbons92407@gmail.com>
2022-11-28 09:01:51 -0700

    added basic auth to admin route

--- web/backend/routes/admin/index.js
+++ web/backend/routes/admin/index.js
@@ -1,8 +1,17 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 import express from 'express'
 const router = express.Router()
 
 import bullBoardRouter from './bull-board.js'
 import apiDocRouter from './api-docs.js'
+import basicAuth from 'express-basic-auth'
+
+router.use(basicAuth({
+  users: { [process.env.ADMIN_USERNAME]: process.env.ADMIN_PASSWORD },
+  challenge: true
+}))
 
 router.use('/admin', bullBoardRouter)
 router.use('/admin', apiDocRouter)
