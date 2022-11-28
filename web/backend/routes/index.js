812808dae412ab1d3cd018c9aa918b2d3c068659
Michael Gibbons <michael.gibbons92407@gmail.com>
2022-11-28 07:50:31 -0700

    added log endpoint

--- web/backend/routes/index.js
+++ web/backend/routes/index.js
@@ -3,11 +3,13 @@ const router = express.Router()
 
 import adminRouter from './admin/index.js'
 import webhookRouter from './webhooks/index.js'
+import logRouter from './log/index.js'
 
 // all root routes *must* be prefixed with `api` since that is the prefix shopify uses for the callback uri during auth.
 // If you do not do this your routes will fail
 
 router.use('/api', adminRouter)
 router.use('/api', webhookRouter)
+router.use('/api', logRouter)
 
 export default router
---
+++ web/backend/routes/log/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+const router = express.Router();
+
+import logger from "../../services/logger/index.js";
+import withSession from '../../middleware/with-session.js'
+
+router.post("/log", express.json(), withSession, async (req, res) => {
+  const { type, message, meta = {} } = req.body;
+
+  meta.shopifySession = res.locals.shopify.session;
+
+  switch (type) {
+    case "error":
+      logger.error(message, meta);
+      break;
+    case "warn":
+      logger.warn(message, meta);
+      break;
+    case "info":
+      logger.info(message, meta);
+      break;
+    case "debug":
+      logger.debug(message, meta);
+      break;
+    case "verbose":
+      logger.verbose(message, meta);
+      break;
+    case "silly":
+      logger.silly(message, meta);
+      break;
+    default:
+      break;
+  }
+
+  res.status(200).json({ success: true });
+});
+
+export default router;
