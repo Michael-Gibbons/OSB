6d6331072b8b1b46c0585fbef09c3576e522885f
Michael Gibbons <michael.gibbons92407@gmail.com>
2023-04-18 16:11:07 -0700

    adding prompts and fixing template bug

--- cli/commands/new/commands/endpoint.js
+++ cli/commands/new/commands/endpoint.js
@@ -3,6 +3,7 @@ const program = new Command();
 
 import fs from 'fs'
 import path from 'path'
+import inquirer from 'inquirer';
 
 const TEMPLATES_PATH = path.join(process.cwd(), '/cli/commands/new/templates')
 
@@ -18,64 +19,81 @@ const endpointCommand =
 program.command('endpoint')
   .description('Creates a new CRUD endpoint.')
   .action((data) => {
-    const pluralizedName = 'dogs'.toLowerCase()
-    const capitalizedPluralName = capitalizeFirstLetter(pluralizedName)
-    const singularName = pluralizedName.substring(0, pluralizedName.length - 1); // Naive implementation, I didn't want to add an entire package to pluralize.
-    const capitalizedSingularName = capitalizeFirstLetter(singularName)
 
-    const isExternalEndpoint = true
-
-    const COLLECTION_TEMPLATE = path.join(TEMPLATES_PATH, 'COLLECTION_TEMPLATE.js')
-    const newCollectionEndpoint = path.join(BACKEND_API_PATH, `/paths/${pluralizedName}.js`)
-
-    const SINGLETON_TEMPLATE = path.join(TEMPLATES_PATH, 'SINGLETON_TEMPLATE.js')
-    const newSingletonEndpoint = path.join(BACKEND_API_PATH, `/paths/${pluralizedName}/{id}.js`)
-
-    const SINGLETON_PATH = path.join(BACKEND_API_PATH, `/paths/${pluralizedName}`);
-
-    if (!fs.existsSync(SINGLETON_PATH)){
-      fs.mkdirSync(SINGLETON_PATH);
-    }else{
-      throw new Error(`"${pluralizedName}" directory already exists.`)
-    }
-
-    const delimiters = [
+    inquirer
+    .prompt([
       {
-        from: "__LOWERCASE_PLURALIZED_NAME__",
-        to: pluralizedName
+        type: "input",
+        message: "Please enter the pluralized name for your resource. (bundles, orders, customers, etc)",
+        name: "name",
       },
       {
-        from: "__UPPERCASE_PLURALIZED_NAME__",
-        to: capitalizedPluralName
-      },
-      {
-        from: "__LOWERCASE_SINGULAR__NAME",
-        to: singularName
-      },
-      {
-        from: "__UPPERCASE_SINGULAR_NAME__",
-        to: capitalizedSingularName
-      },
-      {
-        from: "__IS_EXTERNAL_ENDPOINT__",
-        to: isExternalEndpoint
+        type: "checkbox",
+        message: "Is this an external API endpoint meant to be accessed outside the shopify admin?",
+        name: "isExternalEndpoint",
+        default: false
       }
-    ]
+    ])
+    .then((answers) => {
 
-    copyFileFromTemplate(COLLECTION_TEMPLATE, newCollectionEndpoint, delimiters)
+      const pluralizedName = answers.name.toLowerCase()
+      const capitalizedPluralName = capitalizeFirstLetter(pluralizedName)
+      const singularName = pluralizedName.substring(0, pluralizedName.length - 1); // Naive implementation, I didn't want to add an entire package to pluralize.
+      const capitalizedSingularName = capitalizeFirstLetter(singularName)
 
-    copyFileFromTemplate(SINGLETON_TEMPLATE, newSingletonEndpoint, delimiters)
+      const isExternalEndpoint = answers.isExternalEndpoint
 
-    const INPUT_SCHEMA_TEMPLATE = path.join(TEMPLATES_PATH, 'INPUT_SCHEMA_TEMPLATE.js')
-    const newInputSchema = path.join(BACKEND_API_PATH, `/schemas/inputs/${capitalizedPluralName}Input.js`)
+      const COLLECTION_TEMPLATE = path.join(TEMPLATES_PATH, 'COLLECTION_TEMPLATE.js')
+      const newCollectionEndpoint = path.join(BACKEND_API_PATH, `/paths/${pluralizedName}.js`)
 
-    copyFileFromTemplate(INPUT_SCHEMA_TEMPLATE, newInputSchema, delimiters)
+      const SINGLETON_TEMPLATE = path.join(TEMPLATES_PATH, 'SINGLETON_TEMPLATE.js')
+      const newSingletonEndpoint = path.join(BACKEND_API_PATH, `/paths/${pluralizedName}/{id}.js`)
 
-    const SERVICE_TEMPLATE = path.join(TEMPLATES_PATH, 'SERVICE_TEMPLATE.js')
-    const newService = path.join(BACKEND_API_PATH, `/services/${pluralizedName}Service.js`)
+      const SINGLETON_PATH = path.join(BACKEND_API_PATH, `/paths/${pluralizedName}`);
 
-    copyFileFromTemplate(SERVICE_TEMPLATE, newService, delimiters)
+      if (!fs.existsSync(SINGLETON_PATH)){
+        fs.mkdirSync(SINGLETON_PATH);
+      }else{
+        throw new Error(`"${pluralizedName}" directory already exists.`)
+      }
 
+      const delimiters = [
+        {
+          from: "__LOWERCASE_PLURALIZED_NAME__",
+          to: pluralizedName
+        },
+        {
+          from: "__UPPERCASE_PLURALIZED_NAME__",
+          to: capitalizedPluralName
+        },
+        {
+          from: "__LOWERCASE_SINGULAR__NAME",
+          to: singularName
+        },
+        {
+          from: "__UPPERCASE_SINGULAR_NAME__",
+          to: capitalizedSingularName
+        },
+        {
+          from: "__IS_EXTERNAL_ENDPOINT__",
+          to: isExternalEndpoint
+        }
+      ]
+
+      copyFileFromTemplate(COLLECTION_TEMPLATE, newCollectionEndpoint, delimiters)
+
+      copyFileFromTemplate(SINGLETON_TEMPLATE, newSingletonEndpoint, delimiters)
+
+      const INPUT_SCHEMA_TEMPLATE = path.join(TEMPLATES_PATH, 'INPUT_SCHEMA_TEMPLATE.js')
+      const newInputSchema = path.join(BACKEND_API_PATH, `/schemas/inputs/${capitalizedPluralName}Input.js`)
+
+      copyFileFromTemplate(INPUT_SCHEMA_TEMPLATE, newInputSchema, delimiters)
+
+      const SERVICE_TEMPLATE = path.join(TEMPLATES_PATH, 'SERVICE_TEMPLATE.js')
+      const newService = path.join(BACKEND_API_PATH, `/services/${pluralizedName}Service.js`)
+
+      copyFileFromTemplate(SERVICE_TEMPLATE, newService, delimiters)
+    })
   });
 
 export default endpointCommand
