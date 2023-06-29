7dc95e4611fe0ffc1bae7a4549b4a40ce2562fee
Michael Gibbons <michael.gibbons92407@gmail.com>
2023-06-29 12:35:47 -0600

    updating app error handler

--- web/backend/middleware/error-handler.js
+++ web/backend/middleware/error-handler.js
@@ -1,34 +1,38 @@
 import logger from "../services/logger/index.js"
 
 export default async (err, req, res, next) => {
-  const error = JSON.parse(JSON.stringify(err, Object.getOwnPropertyNames(err)))
+  try {
+    const error = JSON.parse(JSON.stringify(err, Object.getOwnPropertyNames(err)))
 
-  const reqData = {
-    id: req.id,
-    headers: req.headers,
-    method: req.method,
-    url: req.url,
-    httpVersion: req.httpVersion,
-    body: req.body,
-    cookies: req.cookies,
-    path: req.path,
-    protocol: req.protocol,
-    query: req.query,
-    hostname: req.hostname,
-    ip: req.ip,
-    originalUrl: req.originalUrl,
-    params: req.params,
-  };
+    const reqData = {
+      id: req.id,
+      headers: req.headers,
+      method: req.method,
+      url: req.url,
+      httpVersion: req.httpVersion,
+      body: req.body,
+      cookies: req.cookies,
+      path: req.path,
+      protocol: req.protocol,
+      query: req.query,
+      hostname: req.hostname,
+      ip: req.ip,
+      originalUrl: req.originalUrl,
+      params: req.params,
+    };
 
-  const resData = {
-    locals: res.locals
+    const resData = {
+      locals: res.locals
+    }
+
+    logger.error("APP UNHANDLED ERROR", {
+      request: reqData,
+      response: resData,
+      error
+    })
+
+    res.status(500).send("Unhandled Error, a log has been sent to a developer.")
+  } catch (error) {
+    res.status(400).send("Bad Request")
   }
-
-  logger.error("APP UNHANDLED ERROR", {
-    request: reqData,
-    response: resData,
-    error
-  })
-
-  res.status(500).send("Unhandled Error, a log has been sent to a developer.")
 }
